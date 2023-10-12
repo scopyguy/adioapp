7fa9d7356330412914c7ade60573e95da0975bf9
scopyguy <adeniyi20215@gmail.com>
2023-10-12 21:24:52 +0100

    updated file

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -2,7 +2,7 @@ import 'package:adioapp/drawer/drawer.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
